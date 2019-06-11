1f51c46dbc71569c5c4bcaff23ca0d58f429afb0
notoraptor <stevenbocco@gmail.com>
2019-06-11 11:47:34 -0400

    Add /ganify.

---
+++ ganify/precache-manifest.1b925e0713ab6a24a7d7a48c1a215573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9054e6cae6a04a10cb4d7231d693e07d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "005e34a69e50ac9faee5",
+    "url": "./static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1e0356cc84e9d48e3e21",
+    "url": "./static/css/main.cb6c0e50.chunk.css"
+  },
+  {
+    "revision": "005e34a69e50ac9faee5",
+    "url": "./static/js/2.384aa299.chunk.js"
+  },
+  {
+    "revision": "1e0356cc84e9d48e3e21",
+    "url": "./static/js/main.61169474.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
