9f99ad094b3254fd04dcea7eca1d157faedf6a26
notoraptor <stevenbocco@gmail.com>
2019-07-03 16:12:22 -0400

    Add climatepixweb.

---
+++ climatepixweb/precache-manifest.869f191d1be03c043e62e490770e14e7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923d16b7da731c265279fad05223cd13",
+    "url": "./index.html"
+  },
+  {
+    "revision": "95e3fe624e531e10afee",
+    "url": "./static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d26da5138ed29265e33d",
+    "url": "./static/css/main.c0fc5b27.chunk.css"
+  },
+  {
+    "revision": "95e3fe624e531e10afee",
+    "url": "./static/js/2.8a2aed7c.chunk.js"
+  },
+  {
+    "revision": "d26da5138ed29265e33d",
+    "url": "./static/js/main.bdd40e90.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ac6767240be1aa25fcd01b5545a3599b",
+    "url": "./static/media/PublicSans-Bold.ac676724.woff2"
+  },
+  {
+    "revision": "bac72cf359dc6710ff1d00ebc0d84eec",
+    "url": "./static/media/PublicSans-Bold.bac72cf3.woff"
+  },
+  {
+    "revision": "f9d292a11f88095c5671f39eab9014e8",
+    "url": "./static/media/PublicSans-Bold.f9d292a1.ttf"
+  },
+  {
+    "revision": "8629722787c597ea14fb1cb4df422cb0",
+    "url": "./static/media/PublicSans-Regular.86297227.ttf"
+  },
+  {
+    "revision": "87268708bde6e88ac82ea09673eb8bce",
+    "url": "./static/media/PublicSans-Regular.87268708.woff"
+  },
+  {
+    "revision": "a4af6ad9736533ccccd7be506ec85162",
+    "url": "./static/media/PublicSans-Regular.a4af6ad9.woff2"
+  },
+  {
+    "revision": "823bf72da11cc03dfd74aecdf3e7c7a2",
+    "url": "./static/media/PublicSans-SemiBold.823bf72d.woff2"
+  },
+  {
+    "revision": "91732fa28a1baae33699594d285eeec4",
+    "url": "./static/media/PublicSans-SemiBold.91732fa2.ttf"
+  },
+  {
+    "revision": "a4e9d75f1332579d7a24c2b9c266ae96",
+    "url": "./static/media/PublicSans-SemiBold.a4e9d75f.woff"
+  },
+  {
+    "revision": "b6b864f49567e228aef61cfde8168da1",
+    "url": "./static/media/android-app.b6b864f4.png"
+  },
+  {
+    "revision": "57521a7b2d33cb8de2fb439ac27c1f3e",
+    "url": "./static/media/ccai-bg.57521a7b.png"
+  },
+  {
+    "revision": "03cc876ae66b7683bc9f08791dae7a7a",
+    "url": "./static/media/chris-gallagher-4zxp5vlmvnI-unsplash.03cc876a.jpg"
+  },
+  {
+    "revision": "058a14276d2bdb71b4b5c3684070cf58",
+    "url": "./static/media/desmond-simon-e72_wF3daBE-unsplash.058a1427.jpg"
+  },
+  {
+    "revision": "02fc592eaaccaefc92e8b4c01bc671e7",
+    "url": "./static/media/joanne-francis-S9NQnIV4zOI-unsplash.02fc592e.jpg"
+  },
+  {
+    "revision": "5fd853bbde2c59522bff5ba34f3378af",
+    "url": "./static/media/logo.5fd853bb.svg"
+  },
+  {
+    "revision": "df918d3c55bae178bd770e351f1f8c29",
+    "url": "./static/media/marcus-kauffman--iretlQZEU4-unsplash.df918d3c.jpg"
+  }
+]);
