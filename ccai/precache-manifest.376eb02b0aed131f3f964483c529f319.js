d223eb8acda750eaf800b5accc2114a093890bc5
notoraptor <stevenbocco@gmail.com>
2019-09-09 09:50:48 -0400

    up /ccai

---
+++ ccai/precache-manifest.376eb02b0aed131f3f964483c529f319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6f3edabe93c526d031bacccadcb72d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "80aeafd6aa8d8ee90df0",
+    "url": "./static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "66a3b1f46767f267e08d",
+    "url": "./static/css/main.610458ea.chunk.css"
+  },
+  {
+    "revision": "80aeafd6aa8d8ee90df0",
+    "url": "./static/js/2.37f5c34d.chunk.js"
+  },
+  {
+    "revision": "66a3b1f46767f267e08d",
+    "url": "./static/js/main.acd03c3a.chunk.js"
+  },
+  {
+    "revision": "06dd4a728293e39df770",
+    "url": "./static/js/runtime~main.a64c5f38.js"
+  }
+]);
--- ccai/service-worker.js
+++ ccai/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9a0e70771c7434dff9cd7b1b20578ccc.js"
+  "./precache-manifest.376eb02b0aed131f3f964483c529f319.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
